e43bd2c81f13d965cfb30cdeebba918c53f9595b
zdt@macbook <dev@dthappy.com>
2017-03-04 21:46:45 +0800

    install express

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  entry: "./src/app.js",
+  output: {
+    path: __dirname + "/public/dist",
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader',
+        query: {
+        }
+      }
+    ]
+  }
+};
