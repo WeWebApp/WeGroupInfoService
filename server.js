8cb1bbefb8a7b94af570abda16e0a05199159831
ZDT@HP <dev@dthappy.com>
2017-03-06 17:34:43 +0800

    update server.js with routes

--- server.js
+++ server.js
@@ -2,7 +2,34 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
+import routes from './routes';
 
-let app = express();
+const app = express();
+const port = 3000;
 
-app.listen(3000,()=>console.log('Listening on port 3000...')); 
+const server = app.listen(port,(err)=>{
+    console.log('server listen at ' + port);
+});
+
+app.use('/static', express.static(__dirname + '/public'));
+app.use('/dist', express.static(__dirname + '/dist'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(logger('dev'));
+app.use(cookieParser());
+
+app.use((req, res, next) => {
+  //console.log('Time: ', Date.now());
+  console.log('Time: ',new Date());
+  next();
+});
+
+app.use('/api',routes.api);
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: err // {}
+  });
+});
